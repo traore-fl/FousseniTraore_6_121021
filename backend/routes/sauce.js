a3afc8b0946335390b07effb34236c47b9e8bf4b
TRAORE <traore.fuceni@gmail.com>
2021-11-02 00:27:49 +0100

    Modification des routes Post et Put

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,13 +2,14 @@
 const express = require('express');
 const router = express.Router();
 const sauceCtrl = require('../controllers/sauce');
-
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 
 // ROUTES ----------
-router.post('/', auth, sauceCtrl.createSauce);
-router.post('/:id/like',auth,  sauceCtrl.rateSauce);
-router.put('/:id', auth, sauceCtrl.modifySauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.post('/:id/like', auth, multer, sauceCtrl.rateSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
